11948a137e5d91b8b1d66617685a34f69b2b1b3b
rof20004 <rof20004@gmail.com>
2018-01-25 12:42:33 -0400

    Removido método não utilizado

--- api/user/controllers.go
+++ api/user/controllers.go
@@ -63,7 +63,3 @@ func GetUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 }
-
-func sayHello() {
-
-}
